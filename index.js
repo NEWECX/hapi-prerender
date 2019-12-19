11ac67a436d280e60cc1950ed4388370a7afaa5b
GuttermanA <alexanderf.gutterman@gmail.com>
2019-12-19 18:00:59 -0500

    working on proxy solution

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 // Loosely based on [prerender-node](https://github.com/prerender/prerender-node)
 //
 
-var Url = require('url');
-var Zlib = require('zlib');
-var Request = require('request');
-var Hoek = require('hoek');
+var Url = require("url");
+var Zlib = require("zlib");
+var Request = require("request");
+var Hoek = require("hoek");
+var httpProxy = require("http-proxy");
 
 // Declare internals
 var internals = {};
@@ -15,130 +16,146 @@ var internals = {};
 // we support _escaped_fragment_ and want to ensure people aren't
 // penalized for cloaking.
 internals.crawlerUserAgents = [
-  'googlebot',
+  "googlebot",
   // 'yahoo',
   // 'bingbot',
-  'facebot',
-  'slackbot',
-  'baiduspider',
-  'facebookexternalhit',
-  'twitterbot',
-  'rogerbot',
-  'linkedinbot',
-  'embedly',
-  'quora link preview',
-  'showyoubot',
-  'outbrain',
-  'pinterest',
-  'developers.google.com/+/web/snippet',
-  'semrushbot' //SEO tools
+  "facebot",
+  "slackbot",
+  "baiduspider",
+  "facebookexternalhit",
+  "twitterbot",
+  "rogerbot",
+  "linkedinbot",
+  "embedly",
+  "quora link preview",
+  "showyoubot",
+  "outbrain",
+  "pinterest",
+  "developers.google.com/+/web/snippet",
+  "semrushbot" //SEO tools
 ];
 
 internals.extensionsToIgnore = [
-  '.js',
-  '.json',
-  '.css',
-  '.xml',
-  '.less',
-  '.png',
-  '.jpg',
-  '.jpeg',
-  '.gif',
-  '.pdf',
-  '.doc',
-  '.txt',
-  '.ico',
-  '.rss',
-  '.zip',
-  '.mp3',
-  '.rar',
-  '.exe',
-  '.wmv',
-  '.doc',
-  '.avi',
-  '.ppt',
-  '.mpg',
-  '.mpeg',
-  '.tif',
-  '.wav',
-  '.mov',
-  '.psd',
-  '.ai',
-  '.xls',
-  '.mp4',
-  '.m4a',
-  '.swf',
-  '.dat',
-  '.dmg',
-  '.iso',
-  '.flv',
-  '.m4v',
-  '.torrent',
-  '.svg'
+  ".js",
+  ".json",
+  ".css",
+  ".xml",
+  ".less",
+  ".png",
+  ".jpg",
+  ".jpeg",
+  ".gif",
+  ".pdf",
+  ".doc",
+  ".txt",
+  ".ico",
+  ".rss",
+  ".zip",
+  ".mp3",
+  ".rar",
+  ".exe",
+  ".wmv",
+  ".doc",
+  ".avi",
+  ".ppt",
+  ".mpg",
+  ".mpeg",
+  ".tif",
+  ".wav",
+  ".mov",
+  ".psd",
+  ".ai",
+  ".xls",
+  ".mp4",
+  ".m4a",
+  ".swf",
+  ".dat",
+  ".dmg",
+  ".iso",
+  ".flv",
+  ".m4v",
+  ".torrent",
+  ".svg"
 ];
 
-internals.shouldShowPrerenderedPage = function (req) {
-  var userAgent = req.headers['user-agent'];
-  var bufferAgent = req.headers['x-bufferbot'];
+internals.shouldShowPrerenderedPage = function(req) {
+  var userAgent = req.headers["user-agent"];
+  var bufferAgent = req.headers["x-bufferbot"];
   var isRequestingPrerenderedPage = false;
 
-  if (!userAgent) { return false; }
-  if (req.method.toLowerCase() !== 'get') { return false; }
+  if (!userAgent) {
+    return false;
+  }
+  if (req.method.toLowerCase() !== "get") {
+    return false;
+  }
 
   //if it contains _escaped_fragment_, show prerendered page
-  if (typeof req.url.query._escaped_fragment_ !== 'undefined') {
+  if (typeof req.url.query._escaped_fragment_ !== "undefined") {
     isRequestingPrerenderedPage = true;
   }
 
   //if it is a bot...show prerendered page
-  var knownBot = internals.crawlerUserAgents.some(function (crawlerUserAgent) {
-    return userAgent.toLowerCase().indexOf(crawlerUserAgent.toLowerCase()) !== -1;
+  var knownBot = internals.crawlerUserAgents.some(function(crawlerUserAgent) {
+    return (
+      userAgent.toLowerCase().indexOf(crawlerUserAgent.toLowerCase()) !== -1
+    );
   });
-  
-  if (knownBot) { isRequestingPrerenderedPage = true; }
+
+  if (knownBot) {
+    isRequestingPrerenderedPage = true;
+  }
 
   //if it is BufferBot...show prerendered page
-  if (bufferAgent) { isRequestingPrerenderedPage = true; }
+  if (bufferAgent) {
+    isRequestingPrerenderedPage = true;
+  }
 
   //if it is a bot and is requesting a resource...dont prerender
-  var resource = internals.extensionsToIgnore.some(function (extension) {
-    var regexp = new RegExp(extension + '$');
+  var resource = internals.extensionsToIgnore.some(function(extension) {
+    var regexp = new RegExp(extension + "$");
     return regexp.test(req.url.pathname);
   });
 
   //if blacklisted
 
+  //   var blacklisted = settings.blacklist.some(function (setting) {
+  //     var settingRegex = new RegExp(setting);
+  //     return settingRegex.test(req.url.pathname)
+  //   })
 
-//   var blacklisted = settings.blacklist.some(function (setting) {
-//     var settingRegex = new RegExp(setting);
-//     return settingRegex.test(req.url.pathname)
-//   })
-
-  if (resource) { return false; }
+  if (resource) {
+    return false;
+  }
 
   return isRequestingPrerenderedPage;
 };
-
 
 //
 // Public API
 //
 
-const register = function (server, options, next) {
-
-  var settings = Hoek.applyToDefaults({
-    serviceUrl: process.env.PRERENDER_SERVICE_URL || 'http://service.prerender.io/',
-    token: process.env.PRERENDER_TOKEN,
-    protocol: false,
-    beforeRender: function (req, done) { done(); },
-    afterRender: function (req, resp) {},
-    blacklist: [],
-  }, options);
+const register = function(server, options, next) {
+  var settings = Hoek.applyToDefaults(
+    {
+      serviceUrl:
+        process.env.PRERENDER_SERVICE_URL || "http://service.prerender.io/",
+      token: process.env.PRERENDER_TOKEN,
+      protocol: false,
+      beforeRender: function(req, done) {
+        done();
+      },
+      afterRender: function(req, resp) {},
+      blacklist: ["/dist/", "/js/"]
+    },
+    options
+  );
 
   // blacklist:['/dist/', '/js/', 'json']
   function buildApiUrl(req) {
     var prerenderUrl = settings.serviceUrl;
-    var forwardSlash = prerenderUrl.indexOf('/', prerenderUrl.length - 1) !== -1 ? '' : '/';
+    var forwardSlash =
+      prerenderUrl.indexOf("/", prerenderUrl.length - 1) !== -1 ? "" : "/";
 
     // Here we need to look at the request's protocol, not sure if this is
     // correct...
@@ -161,11 +178,11 @@ const register = function (server, options, next) {
   }
 
   function plainResponse(resp, cb) {
-    var content = '';
-    resp.on('data', function (chunk) {
+    var content = "";
+    resp.on("data", function(chunk) {
       content += chunk;
     });
-    resp.on('end', function () {
+    resp.on("end", function() {
       resp.body = content;
       cb(null, resp);
     });
@@ -173,16 +190,16 @@ const register = function (server, options, next) {
 
   function gzipResponse(resp, cb) {
     var gunzip = Zlib.createGunzip();
-    var content = '';
+    var content = "";
 
-    gunzip.on('data', function (chunk) {
+    gunzip.on("data", function(chunk) {
       content += chunk;
     });
 
-    gunzip.on('end', function () {
+    gunzip.on("end", function() {
       resp.body = content;
-      delete resp.headers['content-encoding'];
-      delete resp.headers['content-length'];
+      delete resp.headers["content-encoding"];
+      delete resp.headers["content-length"];
       cb(null, resp);
     });
 
@@ -197,19 +214,19 @@ const register = function (server, options, next) {
 
     if (settings.token) {
       reqOptions.headers = {
-        'X-Prerender-Token': settings.token,
-        'User-Agent': req.headers['user-agent'],
-        'Accept-Encoding': 'gzip'
+        "X-Prerender-Token": settings.token,
+        "User-Agent": req.headers["user-agent"],
+        "Accept-Encoding": "gzip"
       };
     }
 
     Request(reqOptions)
-      .on('error', function (err) {
+      .on("error", function(err) {
         cb(err);
       })
-      .on('response', function (resp) {
-        var encoding = resp.headers['content-encoding'];
-        if (encoding && encoding === 'gzip') {
+      .on("response", function(resp) {
+        var encoding = resp.headers["content-encoding"];
+        if (encoding && encoding === "gzip") {
           gzipResponse(resp, cb);
         } else {
           plainResponse(resp, cb);
@@ -233,30 +250,42 @@ const register = function (server, options, next) {
     });
   }
 
-  server.ext('onRequest', async function (req, h) {
+  server.ext("onRequest", async function(req, h) {
     // Only handle requests with _escaped_fragment_ query param.
-    if (!internals.shouldShowPrerenderedPage(req)) { return h.continue; }
+
+    const shouldShowPrerenderedPage = internals.shouldShowPrerenderedPage(req);
+    if (!shouldShowPrerenderedPage) {
+      return h.continue;
+    }
+
+    if (
+      req.headers &&
+      req.headers.accept &&
+      req.headers.accept.includes("text/css")
+    ) {
+      console.log(req);
+    }
 
     function sendResponse(resp) {
       var r = h.response(resp.body);
       r.code(resp.statusCode);
-      r.type('text/html');
-      Object.getOwnPropertyNames(resp.headers).forEach(function (k) {
+      r.type("text/html");
+      Object.getOwnPropertyNames(resp.headers).forEach(function(k) {
         r.header(k, resp.headers[k]);
       });
       return r;
     }
 
     const { err, cached } = await beforeRenderWrapper(req);
-    if (!err && cached && typeof cached.body === 'string') {
+    if (!err && cached && typeof cached.body === "string") {
       return sendResponse(cached).continue;
     }
 
     const { err2, resp } = await getPrerenderedPageResponseWrapper(req);
     if (err2) {
-      console.error('Error getting prerendered page.');
+      console.error("Error getting prerendered page.");
       console.error(err2);
-      console.error('Falling back to unrendered (normal) reponse...');
+      console.error("Falling back to unrendered (normal) reponse...");
       return h.continue;
     }
 
@@ -272,11 +301,10 @@ const register = function (server, options, next) {
   });
 
   if (next) next();
-
 };
 
 exports.plugin = {
   name: "HapiPrerender",
   register,
-  pkg: require('./package.json')
+  pkg: require("./package.json")
 };
